6a35cde1711c18d201f5a5abb470c9f75ac8e208
WidyBangkitJuniar <widibangkitjuniar@gmail.com>
2024-02-16 15:32:18 +0700

    tugas 1

---
+++ a.php
@@ -0,0 +1,18 @@
+<?php
+
+$barang = "Topi";
+$berbentuk = "Bundar";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><i>"<?php echo $barang ?> saya <?php echo $berbentuk ?>, <?php echo $berbentuk ?> <?php echo $barang ?> saya."</i></h2>
+</body>
+</html>
---
+++ b.php
@@ -0,0 +1,21 @@
+<?php
+
+$angka = "79";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Aku adalah angka <b><?php echo $angka ?></b></p>
+    <p>Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $angka * 5 ?></b></p>
+    <p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $angka / 2 ?></b></p>
+    <p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $angka + 75 ?></b></p>
+    <p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $angka - 20 ?></b></p>
+</body>
+</html>
