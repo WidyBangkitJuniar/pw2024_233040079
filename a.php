121e0c84b15a07897441f9eeb12c703a7e0ac42d
WidyBangkitJuniar <widibangkitjuniar@gmail.com>
2024-02-20 00:09:11 +0700

    mengerjakan tugas 1

--- a.php
+++ a.php
@@ -1,18 +1,8 @@
 <?php
 
 $barang = "Topi";
-$berbentuk = "Bundar";
+$bentuk = "Bundar";
+
+echo "<h2><i> \"$barang saya $bentuk, $bentuk $barang saya\"";
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2><i>"<?php echo $barang ?> saya <?php echo $berbentuk ?>, <?php echo $berbentuk ?> <?php echo $barang ?> saya."</i></h2>
-</body>
-</html>
--- b.php
+++ b.php
@@ -2,20 +2,18 @@
 
 $angka = "79";
 
-?>
+echo "Aku adalah angka <b>$angka</b><br>";
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Aku adalah angka <b><?php echo $angka ?></b></p>
-    <p>Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $angka * 5 ?></b></p>
-    <p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $angka / 2 ?></b></p>
-    <p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $angka + 75 ?></b></p>
-    <p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $angka - 20 ?></b></p>
-</body>
-</html>
+$angka *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka /= "2";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka += "75";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka -= "20";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>$angka</b><br>";
+
+?>
