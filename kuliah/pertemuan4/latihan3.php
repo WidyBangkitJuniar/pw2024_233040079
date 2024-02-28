90d89e4b6bb636a3f9a11cd959a1131cbb09efb8
WidyBangkitJuniar <widibangkitjuniar@gmail.com>
2024-02-28 11:18:03 +0700

    mengerjakan tugas 3

---
+++ kuliah/pertemuan4/latihan3.php
@@ -0,0 +1,14 @@
+<?php
+    function cetak_angka($baris) {
+        for($i = 1; $i <= $baris; $i++) {
+            for($j = 1; $j <= $i; $j++) {
+                echo "$i";
+            }
+            echo "<br>";
+        }
+    }
+
+    cetak_angka(10);
+    echo "<br>";
+    cetak_angka(7);
+?>
